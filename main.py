c9ac233f6f883056c3cfd4066352e39836996146
Otto <ottolaitinen@outlook.com>
2022-02-23 18:11:11 +0200

    def block representations and random initial list

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
 
+    SIDE_PADDING = 100
+    TOP_PADDING = 150
+
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
@@ -21,4 +24,24 @@ class DrawInformation:
         self.set_list(lst)
 
     def set_list(self, lst):
-        pass
+        self.lst = lst
+        self.min_value = min(lst)
+        self.max_value = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PADDING) / len(lst))
+        self.block_height = round(
+            (self.height - self.TOP_PADDING) / (self.max_value - self.min_value)
+        )
+        # starting point of blocks on the x-axis:
+        self.start_x = self.SIDE_PADDING // 2
+
+
+def generate_initial_list(n, min_value, max_value):
+    lst = []
+
+    for _ in range(n):
+        value = random.randint(min_value, max_value)
+        lst.append(value)
+
+    return lst
+
